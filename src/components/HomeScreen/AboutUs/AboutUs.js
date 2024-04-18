b5ec469a30e5a6660c74f94e042c91f0bd4da3b2
houaida-mangour <houaida.mangour@insat.ucar.tn>
2024-04-18 23:00:48 +0100

    upp

--- src/components/HomeScreen/AboutUs/AboutUs.js
+++ src/components/HomeScreen/AboutUs/AboutUs.js
@@ -9,7 +9,7 @@ return (
     <div id="AboutUs">
     <div className="service-section-wrapper">
       <div className="service-section-top">
-        <p className="primary-subheading"> Event Management platform</p>
+        <p className="primary-subheading"> Event Management Platform</p>
         <h1 className="primary-heading">PLURIVENT</h1>
         <p className="primary-text">
         At PLURIVENT, we are passionate about simplifying event management. With a focus on innovation and user experience, we strive to
