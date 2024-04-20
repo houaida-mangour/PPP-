fab38bb8fdd1f1fd4ae2dc37782bfcb818148c68
houaida-mangour <houaida.mangour@insat.ucar.tn>
2024-04-20 01:00:17 +0100

    hou

--- front-end/src/components/AboutUs/AboutUs.js
+++ front-end/src/components/AboutUs/AboutUs.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "./AboutUs.css";
-import event1 from "./event.jpg";
-
+import eventVideo from "./pub.mp4"; 
 
 function AboutUs() {
-
-
     return (
         <div id="AboutUs">
             <div>
                 <div className="Wrap">
                     <p className="primary-subheading"> PLURIVENT</p>
+                    <p className="primary-subheading"> "EVENT MANAGEMENT PLATFORM"</p>
 
-                    <p class="description">
-                        At <mark>PLURIVENT</mark>, we are passionate about simplifying event management. With a focus on innovation and user experience, we strive to
+                    <p className="description">
+                        At <b>PLURIVENT</b>, we are passionate about simplifying event management. With a focus on innovation and user experience, we strive to
                         provide comprehensive solutions that empower event organizers to create unforgettable experiences. Our platform offers intuitive tools
                         for participant registration, streamlined rooming and catering coordination, and effective event promotion. Backed by a dedicated
                         team committed to excellence, we are here to support you every step of the way in bringing your vision to life.
                     </p>
-                    <img src={event1} alt="Illustration of people planning an event" />
+                    <div><video className="eventvideo" src={eventVideo} alt="Illustration of people planning an event" controls></video>
+                    </div>
                 </div>
             </div>
-        </div>);
+        </div>
+    );
 }
 
 export default AboutUs;
