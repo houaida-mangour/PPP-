4dcb6351add682ca359f2416f024fde52ddbfb86
houaida-mangour <houaida.mangour@insat.ucar.tn>
2024-05-03 15:15:29 +0100

    hiii

---
+++ front-end/src/pages/EventPage/EventPage.js
@@ -0,0 +1,10 @@
+import Cardevent from "../../components/Cardevent/CardEvent";
+
+export default function Event() {
+    return (
+        <div>
+            <Cardevent />
+            
+        </div> 
+    );
+}
