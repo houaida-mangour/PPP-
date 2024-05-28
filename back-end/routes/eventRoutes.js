367825ae2fad79c2fc3f6582d46e153f5968a72d
houaida-mangour <123065645+houaida-mangour@users.noreply.github.com>
2024-05-28 09:57:44 +0100

    manageevent

--- back-end/routes/eventRoutes.js
+++ back-end/routes/eventRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { createEvent, getEvents } from '../controllers/eventController.js';
+import { createEvent, getEvents, getEventById, updateEvent, deleteEvent, upload, getEventsByUser } from '../Controllers/eventController.js';
+import { verifyUser } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/createEvent', createEvent);
-router.get('/events', getEvents);
+// Correct the route to match the frontend request
+router.post('/create', verifyUser, upload.single('image'), createEvent);
+router.get('/', getEvents);
+router.get('/:id', getEventById);
+router.get('/user/:userId', verifyUser, getEventsByUser);
+router.put('/:id', verifyUser, updateEvent);
+router.delete('/:id', verifyUser, deleteEvent);
 
 export default router;
